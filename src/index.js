eef704f46f93fc345f8a16e1b25f17d6672b0f91
mklaudia <39603292+mklaudia@users.noreply.github.com>
2018-06-02 12:50:02 +0200

    do depending on what happened as action

--- src/index.js
+++ src/index.js
@@ -2,29 +2,40 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import axios from "axios";
 
-const reducer = (state, action) => {
+const initialState = {
+  fetching: false,
+  fetched: false,
+  posts: [],
+  err: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_POSTS_PENDING":
+      return { ...state, fetching: true };
+  }
   console.log(action.type);
   return state;
 };
 
 const middleware = applyMiddleware(thunk);
 
-const store = createStore(reducer, null, middleware);
+const store = createStore(reducer, middleware);
 
 store.subscribe(() => {
   console.log("store change", store.getState());
 });
 
 store.dispatch(dispatch => {
-  dispatch({ type: "START" });
+  dispatch({ type: "FETCH_POSTS_PENDING" });
   //do sg this is synchronous
   axios
     .get("https://jsonplaceholder.typicode.com/posts")
     .then(respone => {
-      dispatch({ type: "AA" });
+      dispatch({ type: "FETCH_POSTS_FILTERED" });
     })
     .catch(error => {
-      dispatch({ type: "ERROR" });
+      dispatch({ type: "FETCH_POSTS_REJECTED" });
     });
   dispatch({ type: "END" });
 });
