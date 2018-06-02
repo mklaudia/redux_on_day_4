8c1ed7ba062d4a31df3d0df6b701308e74702ab4
mklaudia <39603292+mklaudia@users.noreply.github.com>
2018-06-02 11:49:39 +0200

    NonWorking logger, error

--- src/index.js
+++ src/index.js
@@ -7,16 +7,38 @@ const reducer = (state, action) => {
     case "INC": {
       return (state = +1);
     }
+    case "E": {
+      throw new Error("ouch");
+    }
     default:
       return state;
   }
 };
+/*
+//pass store, to function next to action
+const logger = store => next => action => {
+  console.log = ("action fired", action);
+  next(action);
+};
+*/
 
-const store = createStore(reducer);
+const error = store => next => action => {
+  try {
+    console.log = ("action fired", action);
+    next(action);
+  } catch (e) {
+    console.log = ("Error, ", e);
+  }
+};
+
+const middleware = applyMiddleware(error);
+
+const store = createStore(reducer, null, middleware);
 
 store.subscribe(() => {
   console.log("Store change: ", store.getState());
 });
 
 //this does nothing yet as nothing is listening
-store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC", payload: {} });
+store.dispatch({ type: "E" });
